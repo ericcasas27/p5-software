6633cfea3b18ab4159cd226cc7c3277d9aafb789
Èric Casas <163521660+ericcasas27@users.noreply.github.com>
2025-01-16 12:11:09 +0100

    .

---
+++ cmake-build-debug/CMakeFiles/p5_software.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/Èric/Documents/GitHub/p5-software/Grollera.cpp" "CMakeFiles/p5_software.dir/Grollera.cpp.obj" "gcc" "CMakeFiles/p5_software.dir/Grollera.cpp.obj.d"
+  "C:/Users/Èric/Documents/GitHub/p5-software/Imatge.cpp" "CMakeFiles/p5_software.dir/Imatge.cpp.obj" "gcc" "CMakeFiles/p5_software.dir/Imatge.cpp.obj.d"
+  "C:/Users/Èric/Documents/GitHub/p5-software/MissatgePrivat.cpp" "CMakeFiles/p5_software.dir/MissatgePrivat.cpp.obj" "gcc" "CMakeFiles/p5_software.dir/MissatgePrivat.cpp.obj.d"
+  "C:/Users/Èric/Documents/GitHub/p5-software/Moderador.cpp" "CMakeFiles/p5_software.dir/Moderador.cpp.obj" "gcc" "CMakeFiles/p5_software.dir/Moderador.cpp.obj.d"
+  "C:/Users/Èric/Documents/GitHub/p5-software/NoMostrades.cpp" "CMakeFiles/p5_software.dir/NoMostrades.cpp.obj" "gcc" "CMakeFiles/p5_software.dir/NoMostrades.cpp.obj.d"
+  "C:/Users/Èric/Documents/GitHub/p5-software/Paraula.cpp" "CMakeFiles/p5_software.dir/Paraula.cpp.obj" "gcc" "CMakeFiles/p5_software.dir/Paraula.cpp.obj.d"
+  "C:/Users/Èric/Documents/GitHub/p5-software/Prauala.cpp" "CMakeFiles/p5_software.dir/Prauala.cpp.obj" "gcc" "CMakeFiles/p5_software.dir/Prauala.cpp.obj.d"
+  "C:/Users/Èric/Documents/GitHub/p5-software/TalQual.cpp" "CMakeFiles/p5_software.dir/TalQual.cpp.obj" "gcc" "CMakeFiles/p5_software.dir/TalQual.cpp.obj.d"
+  "C:/Users/Èric/Documents/GitHub/p5-software/Text.cpp" "CMakeFiles/p5_software.dir/Text.cpp.obj" "gcc" "CMakeFiles/p5_software.dir/Text.cpp.obj.d"
+  "C:/Users/Èric/Documents/GitHub/p5-software/UsuariRegistrat.cpp" "CMakeFiles/p5_software.dir/UsuariRegistrat.cpp.obj" "gcc" "CMakeFiles/p5_software.dir/UsuariRegistrat.cpp.obj.d"
+  "C:/Users/Èric/Documents/GitHub/p5-software/Video.cpp" "CMakeFiles/p5_software.dir/Video.cpp.obj" "gcc" "CMakeFiles/p5_software.dir/Video.cpp.obj.d"
+  "C:/Users/Èric/Documents/GitHub/p5-software/main.cpp" "CMakeFiles/p5_software.dir/main.cpp.obj" "gcc" "CMakeFiles/p5_software.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/p5_software.dir/cmake_clean.cmake
@@ -0,0 +1,35 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/p5_software.dir/Grollera.cpp.obj"
+  "CMakeFiles/p5_software.dir/Grollera.cpp.obj.d"
+  "CMakeFiles/p5_software.dir/Imatge.cpp.obj"
+  "CMakeFiles/p5_software.dir/Imatge.cpp.obj.d"
+  "CMakeFiles/p5_software.dir/MissatgePrivat.cpp.obj"
+  "CMakeFiles/p5_software.dir/MissatgePrivat.cpp.obj.d"
+  "CMakeFiles/p5_software.dir/Moderador.cpp.obj"
+  "CMakeFiles/p5_software.dir/Moderador.cpp.obj.d"
+  "CMakeFiles/p5_software.dir/NoMostrades.cpp.obj"
+  "CMakeFiles/p5_software.dir/NoMostrades.cpp.obj.d"
+  "CMakeFiles/p5_software.dir/Paraula.cpp.obj"
+  "CMakeFiles/p5_software.dir/Paraula.cpp.obj.d"
+  "CMakeFiles/p5_software.dir/Prauala.cpp.obj"
+  "CMakeFiles/p5_software.dir/Prauala.cpp.obj.d"
+  "CMakeFiles/p5_software.dir/TalQual.cpp.obj"
+  "CMakeFiles/p5_software.dir/TalQual.cpp.obj.d"
+  "CMakeFiles/p5_software.dir/Text.cpp.obj"
+  "CMakeFiles/p5_software.dir/Text.cpp.obj.d"
+  "CMakeFiles/p5_software.dir/UsuariRegistrat.cpp.obj"
+  "CMakeFiles/p5_software.dir/UsuariRegistrat.cpp.obj.d"
+  "CMakeFiles/p5_software.dir/Video.cpp.obj"
+  "CMakeFiles/p5_software.dir/Video.cpp.obj.d"
+  "CMakeFiles/p5_software.dir/main.cpp.obj"
+  "CMakeFiles/p5_software.dir/main.cpp.obj.d"
+  "libp5_software.dll.a"
+  "p5_software.exe"
+  "p5_software.exe.manifest"
+  "p5_software.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/p5_software.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
